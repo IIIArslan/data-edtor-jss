bd029100b93dd8bffccb3f33d171a115e6671fe1
IIIArslan <kadirarslanunalll@gmail.com>
2025-07-18 15:44:27 +0300

    Update SearchBox.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Search } from "lucide-react"; // Büyüteç ikonu eklendi
 
 function SearchBox({ arama, setArama }) {
   return (
-    <input
-      className="arama-kutusu"
-      type="text"
-      placeholder="Ara"
-      value={arama}
-      onChange={(e) => setArama(e.target.value)}
-    />
+    <div className="arama-kapsayici">
+      <Search className="arama-ikon" size={16} />
+      <input
+        className="arama-kutusu"
+        type="text"
+        placeholder="Ara"
+        value={arama}
+        onChange={(e) => setArama(e.target.value)}
+      />
+    </div>
   );
 }
 
